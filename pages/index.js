b77406d9f53b291acdfaf2d860c3c2407d0b310d
Mastes007 <05ramalnzrv@gmail.com>
2025-08-01 02:58:03 +0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: "#000", color: "#0f0", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <h1>🧠 Welcome to Neurocore - It works!</h1>
+    </div>
+  );
+}
