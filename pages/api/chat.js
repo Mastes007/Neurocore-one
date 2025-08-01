df66ce46e8ed1a4336b981f3fed8d3736a6e3081
Mastes007 <05ramalnzrv@gmail.com>
2025-08-01 21:46:40 +0400

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -3,14 +3,13 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Only POST requests allowed" });
   }
 
-  const apiKey = "sk-proj-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"; // ← Buraya kendi OpenAI API Key’ini yaz
-
+  const apiKey = "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"; // 
   const { message } = req.body;
 
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "appication/json",
       Authorization: `Bearer ${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}`,
     },
     body: JSON.stringify({
