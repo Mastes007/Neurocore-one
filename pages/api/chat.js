bb28f97e7227795852ddae2b78bd6122fc4ee52e
Mastes007 <05ramalnzrv@gmail.com>
2025-08-01 23:00:34 +0400

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -15,6 +15,7 @@ export default async function handler(req, res) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Console.log("Gönderilen header:", {
       Authorization: `Bearer ${apiKey}`, // 
     },
     body: JSON.stringify({
